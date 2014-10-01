b8420661434a61e8851337af43beddd386046951
squid314 <squid314@users.noreply.github.com>
2014-10-01 16:34:01 -0600

    python virtual environments

---
+++ bashrc.d/py-virtualenv.sh
@@ -0,0 +1,24 @@
+# python virtualenv and virtualenvwrapper stuff
+
+# virtualenvwrapper directory
+export WORKON_HOME=~/dev/venvs
+# load the wrapper
+source /usr/local/bin/virtualenvwrapper_lazy.sh
+
+# virtualenv aliases
+# http://blog.doughellmann.com/2010/01/virtualenvwrapper-tips-and-tricks.html (with modifications)
+alias v='workon'
+alias v.d='deactivate'
+#alias v.deactivate='deactivate'
+alias v.mk='mkvirtualenv'
+alias v.rm='rmvirtualenv'
+alias v.tmp='mktmpenv'
+alias v.sw='workon'
+#alias v.switch='workon'
+alias v.add='add2virtualenv'
+#alias v.add2virtualenv='add2virtualenv'
+alias v.cd='cdvirtualenv'
+alias v.cds='cdsitepackages'
+#alias v.cdsitepackages='cdsitepackages'
+alias v.lss='lssitepackages'
+#alias v.lssitepackages='lssitepackages'
