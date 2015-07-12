6807086d5a59d961c8365e3c6ba0044acce5e3e3
squid314 <squid314@users.noreply.github.com>
2015-07-12 17:30:16 -0600

    add config update command

---
+++ bashrc.d/common-config.sh
@@ -0,0 +1,4 @@
+# helpful stuff for working with this common config
+
+# common-config update command
+alias comup="git --work-tree=\"${CONFIG_ROOT}\" --git-dir=\"${CONFIG_ROOT}/.git\" fetch --prune && git --work-tree=\"${CONFIG_ROOT}\" --git-dir=\"${CONFIG_ROOT}/.git\" pull --ff-only"
