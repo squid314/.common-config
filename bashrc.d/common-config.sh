b4d02801d2a24a889a965e22fb8106ecae958b9c
squid314 <squid314@users.noreply.github.com>
2019-03-02 01:41:47 +0000

    fix migration error

--- bashrc.d/common-config.sh
+++ bashrc.d/common-config.sh
@@ -2,8 +2,8 @@
 
 # common-config update command
 confup() {
-    git --work-tree=\"${CONFIG_ROOT}\" \
-        --git-dir=\"${CONFIG_ROOT}/.git\" \
+    git --work-tree="${CONFIG_ROOT}" \
+        --git-dir="${CONFIG_ROOT}/.git" \
         pull --ff-only \
         && \
         source ~/.bashrc
