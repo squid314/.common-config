4145d51b94c0c47b39968804e2dd6f083c99c70e
squid314 <squid314@users.noreply.github.com>
2019-06-24 23:47:55 +0000

    add font test

--- bin/colors.sh
+++ bin/colors.sh
@@ -3,9 +3,14 @@
 for effect in {0..9} ; do
     for _bg in {40..49} ; do
         for _fg in {30..39} ; do
-            printf "\e[$effect;$_bg;${_fg}m"" $effect;$_bg;$_fg""\e[0m"
+            printf '\e[%s;%s;%sm %s;%s;%s\e[0m' $effect $_bg $_fg $effect $_bg $_fg
         done
         echo
     done
     echo
+done
+
+echo 'Fonts?:'
+for font in {10..19} ; do
+    printf '\e[%smABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\e[0m\n' $font
 done
