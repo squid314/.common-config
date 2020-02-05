8696d444244faa91f8240ffb43886523ab57d127
David Bloomquist <david.bloomquist@arisant.com>
2020-02-05 15:29:29 -0700

    run with docker group rather than build it in
    ensure that user and group are created by allowing them to be non-unique

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ fi
 
 set -e
 HOST_PWD="$PWD"
-cd $(dirname $0)
+
 
 if [[ $# -gt 1 && "x$1" = "x-n" ]] ; then
     shift
@@ -14,28 +14,27 @@ if [[ $# -gt 1 && "x$1" = "x-n" ]] ; then
     shift
 fi
 
+DOCKERGROUPID=$(stat -c %g /var/run/docker.sock)
 if [[ -z "$(docker image ls | grep $USER.ide)" || "$1" == "-clean" ]]; then
-    if [[ "$1" == "-clean" ]] ; then
-        shift
-    fi
-    DOCKERGROUPID=$(stat -c %g /var/run/docker.sock)
+    if [[ "$1" == "-clean" ]] ; then shift ; fi
     docker build -t $USER.ide \
-        --build-arg USERID=$(id -u $USER) \
-        --build-arg USERNAME=$USER \
-        --build-arg GROUPNAME=$(id -gn $USER) \
-        --build-arg GROUPID=$(id -g $USER) \
-        --build-arg USERHOME=$HOME \
-        --build-arg DOCKERGROUPID=$DOCKERGROUPID \
-        .
+        --build-arg USERID=$(id -u) \
+        --build-arg USERNAME=$(id -un) \
+        --build-arg GROUPID=$(id -g) \
+        --build-arg GROUPNAME=$(id -gn) \
+        --build-arg USERHOME="$HOME" \
+        - <"$(dirname "$0")/Dockerfile"
 fi
 
 homeroot="$(dirname $HOME)"
-docker run -it \
+docker run \
     --rm \
+    -i -t \
     $container_name \
     -v "/:/mnt/root" \
     -v "$homeroot:$homeroot" \
     -v /var/run/docker.sock:/var/run/docker.sock \
+    --group-add $DOCKERGROUPID \
     -e HOST_PWD="$HOST_PWD" \
     -w "$HOST_PWD" \
     $USER.ide \
