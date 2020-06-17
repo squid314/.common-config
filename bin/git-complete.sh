cec84f03df02f16080ede0824e392a4b6a01e9d5
squid314 <squid314@users.noreply.github.com>
2020-06-17 21:11:41 +0000

    git-complete.sh now immediately commits updates

--- bin/git-complete.sh
+++ bin/git-complete.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/sh -ex
 
 CONFIG_ROOT="`dirname \`dirname $0\``"
 
 curl -sL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o "$CONFIG_ROOT"/git/completion.bash
 curl -sL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o "$CONFIG_ROOT"/git/prompt.sh
+
+git -C "$CONFIG_ROOT" status
+git -C "$CONFIG_ROOT" commit -m 'pull latest git completion and prompt' --patch git/prompt.sh git/completion.bash
