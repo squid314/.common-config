57fa999101d951b1b71be9eacbedc0be5d2e449f
squid314 <squid314@users.noreply.github.com>
2021-01-18 17:24:46 +0000

    git: add graph to all oneline logs
    git: fix and format git-complete.sh

--- bin/git-complete.sh
+++ bin/git-complete.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -ex
 
-CONFIG_ROOT="`dirname \`dirname $0\``"
+CONFIG_ROOT="`cd \`dirname $0\`/.. ; pwd`"
 
 curl -sL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o "$CONFIG_ROOT"/git/completion.bash
-curl -sL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o "$CONFIG_ROOT"/git/prompt.sh
+curl -sL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh       -o "$CONFIG_ROOT"/git/prompt.sh
 
 git -C "$CONFIG_ROOT" status
-git -C "$CONFIG_ROOT" commit -m 'pull latest git completion and prompt' --patch git/prompt.sh git/completion.bash
+git -C "$CONFIG_ROOT" commit -m 'git: pull latest completion and prompt' git/prompt.sh git/completion.bash
