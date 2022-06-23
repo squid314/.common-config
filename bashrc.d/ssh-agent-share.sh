83be4d9b9286b41e8c324fc0ae85f9e738797b67
squid314 <squid314@users.noreply.github.com>
2022-06-23 23:05:33 +0000

    sshagent: fix verify script

--- bashrc.d/ssh-agent-share.sh
+++ bashrc.d/ssh-agent-share.sh
@@ -88,7 +88,7 @@ rm -f $AGENT_INFO_FILE $AGENT_INFO_DIR/sh-*
             if [ -z "$SSH_AGENT_PID" ] || \
                     [ -z "$SSH_AUTH_SOCK" ] || \
                     [ ! -S $SSH_AUTH_SOCK ] || \
-                    bash -c "source $AGENT_INFO_FILE && [ \"\$SSH_AGENT_PID\" = \"$SSH_AGENT_PID\" ] && [ \"\$SSH_AUTH_SOCK\" = \"$SSH_AUTH_SOCK\" ]" || \
+                    bash -c "source $AGENT_INFO_FILE"' && [[ "$SSH_AGENT_PID" != "$1" || "$SSH_AUTH_SOCK" != "$2" ]]' - "$SSH_AGENT_PID" "$SSH_AUTH_SOCK" || \
                     ! ps -ef | awk '{print $2}' | grep "$SSH_AGENT_PID" &>/dev/null || \
                     ! bash -c "ssh-add -l ; [ \$? = 2 ] && exit 1 || exit 0" &>/dev/null || \
                     [ ! -f "$AGENT_INFO_DIR/sh-$$" ]
