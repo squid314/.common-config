06117ad6d999068818548dafcbf1838b4943b6f6
squid314 <squid314@users.noreply.github.com>
2023-08-02 17:19:09 +0000

    devenv: add bash setting to control docker/podman
    attempt to support podman (doesn't work on mac os as desired due to vm
    separation).

--- bashrc.d/devenvs.sh
+++ bashrc.d/devenvs.sh
@@ -2,18 +2,40 @@
 
 if ! type docker &>/dev/null && ! type podman &>/dev/null ; then return ; fi
 
-# default to using docker if it is present
-if type docker &>/dev/null ; then
-    __dev_cont() { docker "$@"; }
-else
-    # TODO realistically, need to validate that devenv works in podman
-    __dev_cont() { podman "$@"; }
-fi
+__dev_cont() {
+    # if tool is selected, use that one
+    if [[ $(bget bashrc.d.devenvs.sh.tool) ]] ; then
+        bget bashrc.d.devenvs.sh.tool
+    else
+        # otherwise, detect available tool and default to docker
+        if type docker &>/dev/null ; then
+            echo docker
+        else
+            echo podman
+        fi
+    fi
+}
 
 dev() {
     local tag="$(bget bashrc.d.devenvs.sh.family)"
     if [[ -z $tag ]] ; then tag=redhat8 ; fi
     case "$1" in
+        t|tool)
+            case "$2" in
+                p|pod|podman)  tool=podman ;;
+                d|dock|docker) tool=docker ;;
+                unset)         tool= ;;
+                "")
+                    if [[ $(bget bashrc.d.devenvs.sh.tool) ]] ; then
+                        echo "Current tool: $(bget bashrc.d.devenvs.sh.tool)"
+                    else
+                        echo "Current tool: <unset> (evaluated: $(__dev_cont))"
+                    fi
+                    return 0 ;;
+                *) echo "Invalid tool '$2'." ; return 1 ;;
+            esac
+            bset bashrc.d.devenvs.sh.tool $tool
+            ;;
         f|fam|family)
             case "$2" in
                 c7|cent7|centos7)     family=centos7 ;;
@@ -30,13 +52,13 @@ dev() {
             esac
             bset bashrc.d.devenvs.sh.family $family
             ;;
-        pull) __dev_cont pull quay.io/squid314/devenv:$tag ;;
+        pull) $(__dev_cont) pull quay.io/squid314/devenv:$tag ;;
         start)
-            if [[ "$(__dev_cont container ls -aqf name=devenv-$tag | wc -l)" -eq 1 ]] ; then
-                __dev_cont start devenv-$tag &>/dev/null
+            if [[ "$($(__dev_cont) container ls -aqf name=devenv-$tag | wc -l)" -eq 1 ]] ; then
+                $(__dev_cont) start devenv-$tag &>/dev/null
             else
-                if [[ -z "$(__dev_cont image ls -q quay.io/squid314/devenv:$tag)" ]] ; then
-                    __dev_cont image pull quay.io/squid314/devenv:$tag || return 1
+                if [[ -z "$($(__dev_cont) image ls -q quay.io/squid314/devenv:$tag)" ]] ; then
+                    $(__dev_cont) image pull quay.io/squid314/devenv:$tag || return 1
                 fi
                 local dev devs=()
                 for dev in $(cd $HOME/dev 2>/dev/null && find * -type d -prune) ; do
@@ -44,12 +66,19 @@ dev() {
                         devs=("${devs[@]}" -v "$HOME/dev/$dev:/home/squid/dev/$dev")
                     fi
                 done
-                if   [[ -S /var/run/docker.sock         ]] ; then dock_sock="-v         /var/run/docker.sock:/var/run/docker.sock"
-                elif [[ -S /private/var/run/docker.sock ]] ; then dock_sock="-v /private/var/run/docker.sock:/var/run/docker.sock"
+                if [[ $(__dev_cont) = docker ]] ; then
+                    if   [[ -S /var/run/docker.sock         ]] ; then dock_sock="-v         /var/run/docker.sock:/var/run/docker.sock"
+                    elif [[ -S /private/var/run/docker.sock ]] ; then dock_sock="-v /private/var/run/docker.sock:/var/run/docker.sock"
+                    fi
                 fi
-                __dev_cont run -d --init \
+                if [[ $(__dev_cont) = podman ]] ; then
+                    # may work on linux; doesn't work on mac os with vm separation
+                    userns="--userns=keep-id"
+                fi
+                $(__dev_cont) run -d --init \
                     --name devenv-$tag \
                     ${dock_sock} \
+                    ${userns} \
                     -v squid-dev:/home/squid/dev \
                     "${devs[@]}" \
                     quay.io/squid314/devenv:$tag \
@@ -57,13 +86,13 @@ dev() {
             fi
             ;;
         stop)
-            if [[ "$(__dev_cont container ls -qf name=devenv-$tag | wc -l)" -eq 1 ]] ; then
-                __dev_cont stop devenv-$tag &>/dev/null
+            if [[ "$($(__dev_cont) container ls -qf name=devenv-$tag | wc -l)" -eq 1 ]] ; then
+                $(__dev_cont) stop devenv-$tag &>/dev/null
             fi
             ;;
         rm)
-            if [[ "$(__dev_cont container ls -aqf name=devenv-$tag | wc -l)" -eq 1 ]] ; then
-                __dev_cont container rm -f devenv-$tag &>/dev/null
+            if [[ "$($(__dev_cont) container ls -aqf name=devenv-$tag | wc -l)" -eq 1 ]] ; then
+                $(__dev_cont) container rm -f devenv-$tag &>/dev/null
             fi
             ;;
         clean) dev stop && dev rm ;;
@@ -71,9 +100,9 @@ dev() {
             shift
             if [[ $# -eq 0 ]] ; then
                 # pull in the default cmd for the image
-                set - $(__dev_cont image inspect --format '{{join .Config.Cmd " "}}' quay.io/squid314/devenv:$tag)
+                set - $($(__dev_cont) image inspect --format '{{join .Config.Cmd " "}}' quay.io/squid314/devenv:$tag)
             fi
-            __dev_cont exec -it \
+            $(__dev_cont) exec -it \
                 devenv-$tag \
                 "$@"
             ;;
